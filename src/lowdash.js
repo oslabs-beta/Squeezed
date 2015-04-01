c87ef363497010f910b8dbfaf2d75ba535f5b82d
Alex Zai <azai91@gmail.com>
2015-04-01 09:38:57 -0700

    (feat) add empty methods to src

---
+++ src/lowdash.js
@@ -0,0 +1,145 @@
+var _ = {};
+
+// Returns the first element of an array.
+// Returns undefined if array is empty
+_.first = function(array) {
+
+}
+
+// Returns the last element of an array
+_.last = function(array) {
+
+}
+
+
+_.forEach = function(collection, iteratee) {
+
+}
+
+_.forEachRight = function(colllection, iteratee) {
+
+}
+
+_.indexOf = function(array, value) {
+
+}
+
+_.filter = function(collection, predicate) {
+
+}
+
+_.reject = function(collection, predicate) {
+
+}
+
+_.uniq = function(array) {
+
+}
+
+_.map = function(collection, iteratee) {
+
+}
+
+_.pluck = function(collection, key) {
+
+}
+
+_.invoke = function() {
+
+}
+
+_.reduce = function() {
+
+}
+
+_.reduceRight = function() {
+
+}
+
+_.contains = function() {
+
+}
+
+_.every = function() {
+
+}
+
+_.some = function() {
+
+}
+
+_.extend = function() {
+
+}
+
+_.defaults = function() {
+
+}
+
+_.once = function() {
+
+}
+
+_.memoize = function() {
+
+}
+
+_.delay = function() {
+
+}
+
+_.flatten = function() {
+
+}
+
+_.throttle = function() {
+
+}
+
+_.clone = function() {
+
+}
+
+_.cloneDeep = function() {
+
+}
+
+_.drop = function() {
+
+}
+
+_.dropRight = function() {
+
+}
+
+_.take = function() {
+
+}
+
+_.takeRight = function() {
+
+}
+
+_.includes = function() {
+
+}
+
+_.isArray = function() {
+
+}
+
+_.isNumber = function() {
+
+}
+
+_.isString = function() {
+
+}
+
+_.isFunction = function() {
+
+}
+
+_.size = function() {
+
+}
