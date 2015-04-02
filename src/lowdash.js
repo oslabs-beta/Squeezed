a67ca4ca966d646cb74b52e8d9e710efebc2331c
Alex Zai <azai91@gmail.com>
2015-04-02 00:30:05 -0700

    (fix) update dropRight

--- src/lowdash.js
+++ src/lowdash.js
@@ -67,7 +67,7 @@ _.drop = function(array, n) {
 
 }
 
-// Creates a slice of array with n elements dropped from the end.
+// Creates a slice of array with n elements dropped from the end. n defaults to 1
 // _.dropRight([1, 2, 3]); → [1, 2]
 // _.dropRight([1, 2, 3], 2); → [1]
 // _.dropRight([1, 2, 3], 5); → []
