d4cf957cb851b72deed84b629b27e4831d655ac0
Alex Zai <azai91@gmail.com>
2015-04-01 21:47:37 -0700

    (feat) add mocha tests

--- spec/lowDashSpec.js
+++ spec/lowDashSpec.js
@@ -1,6 +1,564 @@
 describe('Identity', function() {
+  var uniqueObject = {};
 
+  it('should return whatever value is passed into it', function() {
+    expect(_.identity(1)).to.equal(1);
+    expect(_.identity('string')).to.equal('string');
+    expect(_.identity(false)).to.equal(false);
+    expect(_.identity(uniqueObject)).to.equal(uniqueObject);
+  });
 
+});
 
+describe('isNumber', function() {
+  it('should return true for numbers', function() {
+    expect(_.isNumber(0)).to.be.true;
+    expect(_.isNumber(-1)).to.be.true;
+    expect(_.isNumber(1)).to.be.true;
+  });
 
+  it('should return false for everything else', function() {
+    expect(_.isNumber('a')).to.be.false;
+    expect(_.isNumber({})).to.be.false;
+    expect(_.isNumber([])).to.be.false;
+    expect(_.isNumber(true)).to.be.false;
+  });
+});
+
+describe('isString', function() {
+  it('should return true for strings', function() {
+    expect(_.isString('a')).to.be.true;
+    expect(_.isString('')).to.be.true;
+    expect(_.isString('hello')).to.be.true;
+  });
+
+  it('should return false for everything else', function() {
+    expect(_.isString({})).to.be.false;
+    expect(_.isString([])).to.be.false;
+    expect(_.isString(true)).to.be.false;
+    expect(_.isString(1)).to.be.false;
+  });
+});
+
+describe('isArray', function() {
+  it('should return true for strings', function() {
+    expect(_.isArray([])).to.be.true;
+    expect(_.isArray([1])).to.be.true;
+  });
+
+  it('should return false for everything else', function() {
+    expect(_.isArray({})).to.be.false;
+    expect(_.isArray('a')).to.be.false;
+    expect(_.isArray(true)).to.be.false;
+    expect(_.isArray(1)).to.be.false;
+  });
+});
+
+describe('size', function() {
+  it('should return the correct size of the array', function() {
+    expect(_.size([])).to.eql(0);
+    expect(_.size([1])).to.eql(1);
+    expect(_.size({a:1,b:2})).to.eql(2);
+    expect(_.size({})).to.eql(0);
+  })
+});
+
+describe('first', function() {
+  it('should be able to pull out the first element of an array', function() {
+    expect(_.first([1,2,3])).to.equal(1);
+    expect(_.first([6])).to.equal(6);
+  });
+
+  it('should return undefined for empty array', function() {
+    expect(_.first([])).to.be.undefined;
+  });
+});
+
+describe('last', function() {
+  it('should be able to pull out the last element of an array', function() {
+    expect(_.last([1,2,3])).to.equal(3);
+    expect(_.last([6])).to.equal(6);
+  });
+
+  it('should return undefined for empty array', function() {
+    expect(_.last([])).to.be.undefined;
+  });
+});
+
+describe('forEach', function() {
+  it('should iterate over arrays, providing access to the element, index, and array itself', function() {
+    var animals = ['ant', 'bat', 'cat'];
+    var iterationInputs = [];
+
+    _.forEach(animals, function(animal, index, list) {
+      iterationInputs.push([animal, index, list]);
+    });
+
+    expect(iterationInputs).to.eql([
+      ['ant', 0, animals],
+      ['bat', 1, animals],
+      ['cat', 2, animals]
+    ]);
+  });
+
+  it('should only iterate over the array elements, not properties of the array', function() {
+    var animals = ['ant', 'bat', 'cat'];
+    var iterationInputs = [];
+
+    animals.shouldBeIgnored = 'Ignore me!';
+
+    _.forEach(animals, function(animal, index, list) {
+      iterationInputs.push([animal, index, list]);
+    });
+
+    expect(iterationInputs).to.eql([
+      ['ant', 0, animals],
+      ['bat', 1, animals],
+      ['cat', 2, animals]
+    ]);
+  });
+
+  it('should iterate over objects, providing access to the element, index, and object itself', function() {
+    var animals = { a: 'ant', b: 'bat', c: 'cat' };
+    var iterationInputs = [];
+
+    _.forEach(animals, function(animal, key, object) {
+      iterationInputs.push([animal, key, object]);
+    });
+
+    expect(iterationInputs).to.eql([
+      ['ant', 'a', animals],
+      ['bat', 'b', animals],
+      ['cat', 'c', animals]
+    ]);
+  });
+});
+
+describe('forEach', function() {
+  it('should iterate over arrays, providing access to the element, index, and array itself', function() {
+    var animals = ['ant', 'bat', 'cat'];
+    var iterationInputs = [];
+
+    _.forEach(animals, function(animal, index, list) {
+      iterationInputs.push([animal, index, list]);
+    });
+
+    expect(iterationInputs).to.eql([
+      ['ant', 0, animals],
+      ['bat', 1, animals],
+      ['cat', 2, animals]
+    ]);
+  });
+
+  it('should only iterate over the array elements, not properties of the array', function() {
+    var animals = ['ant', 'bat', 'cat'];
+    var iterationInputs = [];
+
+    animals.shouldBeIgnored = 'Ignore me!';
+
+    _.forEach(animals, function(animal, index, list) {
+      iterationInputs.push([animal, index, list]);
+    });
+
+    expect(iterationInputs).to.eql([
+      ['ant', 0, animals],
+      ['bat', 1, animals],
+      ['cat', 2, animals]
+    ]);
+  });
+
+  it('should iterate over objects, providing access to the element, index, and object itself', function() {
+    var animals = { a: 'ant', b: 'bat', c: 'cat' };
+    var iterationInputs = [];
+
+    _.forEach(animals, function(animal, key, object) {
+      iterationInputs.push([animal, key, object]);
+    });
+
+    expect(iterationInputs).to.eql([
+      ['ant', 'a', animals],
+      ['bat', 'b', animals],
+      ['cat', 'c', animals]
+    ]);
+  });
+});
+
+describe('forEachRight', function() {
+  it('should iterate over arrays, providing access to the element, index, and array itself', function() {
+    var animals = ['ant', 'bat', 'cat'];
+    var iterationInputs = [];
+
+    _.forEachRight(animals, function(animal, index, list) {
+      iterationInputs.push([animal, index, list]);
+    });
+
+    expect(iterationInputs).to.eql([
+      ['ant', 0, animals],
+      ['bat', 1, animals],
+      ['cat', 2, animals]
+    ].reverse());
+  });
+
+  it('should only iterate over the array elements, not properties of the array', function() {
+    var animals = ['ant', 'bat', 'cat'];
+    var iterationInputs = [];
+
+    animals.shouldBeIgnored = 'Ignore me!';
+
+    _.forEachRight(animals, function(animal, index, list) {
+      iterationInputs.push([animal, index, list]);
+    });
+
+    expect(iterationInputs).to.eql([
+      ['ant', 0, animals],
+      ['bat', 1, animals],
+      ['cat', 2, animals]
+    ].reverse());
+  });
+
+  it('should iterate over objects, providing access to the element, index, and object itself', function() {
+    var animals = { a: 'ant', b: 'bat', c: 'cat' };
+    var iterationInputs = [];
+
+    _.forEachRight(animals, function(animal, key, object) {
+      iterationInputs.push([animal, key, object]);
+    });
+
+    expect(iterationInputs).to.eql([
+      ['ant', 'a', animals],
+      ['bat', 'b', animals],
+      ['cat', 'c', animals]
+    ].reverse());
+  });
+});
+
+describe('map', function() {
+  it('should apply a function to every value in an array', function() {
+    var doubled = _.map([1, 2, 3], function(num) {
+      return num * 2;
+    });
+
+    expect(doubled).to.eql([2, 4, 6]);
+  });
+});
+
+describe('indexOf', function() {
+  it('should have 40 in the list', function() {
+    var numbers = [10, 20, 30, 40, 50];
+
+    expect(_.indexOf(numbers, 40)).to.be(3);
+  });
+
+  it('should be able to compute indexOf even when the native function is undefined', function() {
+    var numbers = [10, 20, 30];
+
+    // If the browser provides a native indexOf array method, disable it
+    numbers.indexOf = null;
+
+    expect(_.indexOf(numbers, 20)).to.be(1);
+  });
+
+  it('returns -1 when the target cannot be found not in the list', function() {
+    var numbers = [10, 20, 30, 40, 50];
+
+    expect(_.indexOf(numbers, 35)).to.be(-1);
+  });
+
+  it('returns the first index that the target can be found at when there are multiple matches', function() {
+    var numbers = [1, 40, 40, 40, 40, 40, 40, 40, 50, 60, 70];
+
+    expect(_.indexOf(numbers, 40)).to.be(1);
+  });
+});
+
+describe('filter', function() {
+  it('should return all even numbers in an array', function() {
+    var isEven = function(num) { return num % 2 === 0; };
+    var evens = _.filter([1, 2, 3, 4, 5, 6], isEven);
+
+    expect(evens).to.eql([2, 4, 6]);
+  });
+
+  it('should return all odd numbers in an array', function() {
+    var isOdd = function(num) { return num % 2 !== 0; };
+    var odds = _.filter([1, 2, 3, 4, 5, 6], isOdd);
+
+    expect(odds).to.eql([1, 3, 5]);
+  });
+});
+
+describe('reject', function() {
+  it('should reject all even numbers', function() {
+    var isEven = function(num) { return num % 2 === 0; };
+    var odds = _.reject([1, 2, 3, 4, 5, 6], isEven);
+
+    expect(odds).to.eql([1, 3, 5]);
+  });
+
+  it('should reject all odd numbers', function() {
+    var isOdd = function(num) { return num % 2 !== 0; };
+    var evens = _.reject([1, 2, 3, 4, 5, 6], isOdd);
+
+    expect(evens).to.eql([2, 4, 6]);
+  });
+});
+
+describe('uniq', function() {
+  it('should return all unique values contained in an unsorted array', function() {
+    var list = [1, 2, 1, 3, 1, 4];
+
+    expect(_.uniq(list)).to.eql([1, 2, 3, 4]);
+  });
+
+  it('should handle iterators that work with a sorted array', function() {
+    var iterator = function(value) { return value +1; };
+    var list = [1, 2, 2, 3, 4, 4];
+
+    expect(_.uniq(list, true, iterator)).to.eql([1, 2, 3, 4]);
+  });
+});
+
+describe('pluck', function() {
+  it('should return values contained at a user-defined property', function() {
+    var people = [
+      {name : 'moe', age : 30},
+      {name : 'curly', age : 50}
+    ];
+
+    expect(_.pluck(people, 'name')).to.eql(['moe', 'curly']);
+  });
+});
+
+describe('reduce', function() {
+  it('should be able to sum up an array', function() {
+    var add = function(tally, item) {return tally + item; };
+    var total = _.reduce([1, 2, 3], add, 0);
+
+    expect(total).to.equal(6);
+  });
+
+  it('should be able to find the difference in an array', function() {
+    var difference = function(tally, item) {return tally - item; };
+    var total = _.reduce([1, 2, 3], difference, 0);
+
+    expect(total).to.equal(-6);
+  });
+
+  it('should default to the last item in the array', function() {
+    var difference = function(tally, item) {return tally - item; };
+    var total = _.reduce([1, 2, 3], difference);
+
+    expect(total).to.equal(-4);
+  });
+
+  it('should default to the first item in the array', function() {
+    var add = function(tally, item) {return tally + item; };
+    var total = _.reduce([1, 2, 3], add);
+
+    expect(total).to.equal(6);
+  });
+
+});
+
+describe('reduceRight', function() {
+  it('should be able to find the difference in an array', function() {
+    var difference = function(tally, item) {return tally - item; };
+    var total = _.reduceRight([1, 2, 3], difference, 0);
+
+    expect(total).to.equal(-6);
+  });
+
+  it('should default to the last item in the array', function() {
+    var difference = function(tally, item) {return tally - item; };
+    var total = _.reduceRight([1, 2, 3], difference);
+
+    expect(total).to.equal(0);
+  });
+
+});
+
+describe('flatten', function() {
+  it('can flatten nested arrays', function() {
+    expect(_.flatten([1, [2, 3, [4]]])).to.eql([1, 2, 3, [4]]);
+  });
+});
+
+describe('flattenDeep', function() {
+  it('can flatten nested arrays', function() {
+    expect(_.flattenDeep([1, [2, 3, [4]]])).to.eql([1, 2, 3, 4]);
+  });
+});
+
+describe('drop', function() {
+  it('can drop', function() {
+    expect(_.drop([1, 2, 3])).to.eql([2, 3]);
+    expect(_.drop([1, 2, 3], 2)).to.eql([3]);
+    expect(_.drop([1, 2, 3], 5)).to.eql([]);
+    expect(_.drop([1, 2, 3], 0)).to.eql([1, 2, 3]);
+  });
+});
+
+describe('dropRight', function() {
+  it('can drop', function() {
+    expect(_.dropRight([1, 2, 3])).to.eql([1, 2]);
+    expect(_.dropRight([1, 2, 3], 2)).to.eql([1]);
+    expect(_.dropRight([1, 2, 3], 5)).to.eql([]);
+    expect(_.dropRight([1, 2, 3], 0)).to.eql([1, 2, 3]);
+  });
+});
+
+describe('take', function() {
+  it('can take', function() {
+    expect(_.take([1, 2, 3])).to.eql([1]);
+    expect(_.take([1, 2, 3], 2)).to.eql([1,2]);
+    expect(_.take([1, 2, 3], 5)).to.eql([1,2,3]);
+    expect(_.take([1, 2, 3], 0)).to.eql([]);
+  });
+});
+
+describe('extend', function() {
+  it('returns the first argument', function() {
+    var to = {};
+    var from = {};
+    var extended = _.extend(to, from);
+
+    expect(extended).to.equal(to);
+  });
+
+  it('should extend an object with the attributes of another', function() {
+    var to = {};
+    var from = {a:'b'};
+    var extended = _.extend(to, from);
+
+    expect(extended.a).to.equal('b');
+  });
+
+  it('should override properties found on the destination', function() {
+    var to = {a:'x'};
+    var from = {a:'b'};
+    var extended = _.extend(to, from);
+
+    expect(extended.a).to.equal('b');
+  });
+
+  it('should not override properties not found in the source', function() {
+    var to = {x:'x'};
+    var from = {a:'b'};
+    var extended = _.extend(to, from);
+
+    expect(extended.x).to.equal('x');
+  });
+
+  it('should extend from multiple source objects', function() {
+    var extended = _.extend({x:1}, {a:2}, {b:3});
+
+    expect(extended).to.eql({x:1, a:2, b:3});
+  });
+
+  it('in the case of a conflict, it should use the last property\'s values when extending from multiple source objects', function() {
+    var extended = _.extend({x:'x'}, {a:'a', x:2}, {a:1});
+
+    expect(extended).to.eql({x:2, a:1});
+  });
+
+  it('should copy undefined values', function() {
+    var extended = _.extend({}, {a: void 0, b: null});
+
+    expect('a' in extended && 'b' in extended).to.be(true);
+  });
+});
+
+describe('clone', function() {
+  it('should clone', function() {
+    var users = [{ 'user': 'barney' },{ 'user': 'fred' }];
+    var shallowClone = _.clone(users);
+    expect(shallowClone[0].user).to.equal(users[0].user);
+    expect(shallowClone === users).to.be.true;
+  });
+});
+
+describe('cloneDeep', function() {
+  it('should ', function() {
+    var users = [{ 'user': 'barney' },{ 'user': 'fred' }];
+    var deepClone = _.cloneDeep(users);
+    expect(deepClone[0].user).to.equal(users[0].user);
+    expect(deepClone).to.not.equal(users);
+    expect(deepClone).to.eql(users);
+  });
+});
+
+describe('once', function() {
+  it('should only run a user-defined function if it hasn\'t been run before', function() {
+    var num = 0;
+    var increment = _.once(function() {
+      num += 1;
+    });
+
+    increment();
+    increment();
+
+    expect(num).to.equal(1);
+  });
+});
+
+describe('memoize', function() {
+  var fib, fastFib, timeCheck, fastTime, wait;
+
+  beforeEach(function() {
+    fib = function(n) {
+      if(n < 2){ return n; }
+      return fib(n - 1) + fib(n - 2);
+    };
+    fastFib = _.memoize(fib);
+
+    timeCheck = function(str) { return str + Date.now(); };
+    fastTime = _.memoize(timeCheck);
+
+    // Synchronous sleep: terrible for web development, awesome for testing _.memoize
+    wait = function(t) {
+      var start = Date.now();
+      while ((Date.now() - start) < t){}
+    };
+  });
+
+  it('a memoized function should produce the same result when called with the same arguments', function() {
+    expect(fib(10)).to.equal(55);
+    expect(fastFib(10)).to.equal(55);
+  });
+
+  it('should give different results for different arguments', function() {
+    expect(fib(10)).to.equal(55);
+    expect(fastFib(10)).to.equal(55);
+    expect(fastFib(7)).to.equal(13);
+  });
+
+  it('should not run the function twice for the same given argument', function() {
+    var firstTime = timeCheck('shazaam!');
+    wait(5);
+    var secondTime = fastTime('shazaam!');
+    wait(5);
+    expect(firstTime).to.not.equal(secondTime);
+    expect(fastTime('shazaam!')).to.equal(secondTime);
+  });
+});
+
+describe('delay', function() {
+  var clock;
+
+  beforeEach(function() {
+    clock = sinon.useFakeTimers();
+  });
+
+  afterEach(function() {
+    clock.restore();
+  });
+
+  it('should only execute the function after the specified wait time', function() {
+    var callback = sinon.spy();
+    _.delay(callback, 100);
+    clock.tick(99);
+    expect(callback.notCalled).to.be.true;
+    clock.tick(1);
+    expect(callback.calledOnce).to.be.true;
+  });
 });
--- src/lowdash.js
+++ src/lowdash.js
@@ -2,26 +2,35 @@ var _ = {};
 
 // Utilities
 
-_.identity = function() {
+// Returns the first argument provided to it
+_.identity = function(value) {
 
 }
 
-_.isArray = function() {
-
-}
-
+// Returns boolean of whether argument is classified as a Number object
+// _.isNumber(5); → true
+// _.isNumber('hi'); → false
 _.isNumber = function() {
 
 }
 
+// Returns boolean of whether argument is classified as a String object
+// _.isString('hi'); → true
+// _.isString(5); → false
 _.isString = function() {
 
 }
 
-_.isFunction = function() {
+// Returns boolean of whether argument is classified as an Array object
+// _.isArray(5); → false
+// _.isNumber([1,2,3]); → true
+_.isArray = function(value) {
 
 }
 
+// Return the size of collection by returning its length for array-like values or the number of own enumerable properties for objects.
+// _.size([1,2,3]); → 3
+// _.size({a: 1, b: 2}); → 2
 _.size = function() {
 
 }
@@ -30,127 +39,220 @@ _.size = function() {
 
 // Returns the first element of an array.
 // Returns undefined if array is empty
+// _.first([1,2,3]); → 1
+// _.first([]); → undefined
 _.first = function(array) {
 
 }
 
 // Returns the last element of an array
+// _.last([1,2,3]); → 3
+// _.last([]); → undefined
 _.last = function(array) {
 
 }
 
 //Array
 
-_.forEach = function(collection, iteratee) {
+// Iterates over elements of collection invoking callback for each element.
+// Callback(element/value, index/key, collection)
+// _.forEach(['a','b','c'], function(element, index, collection) {
+//  console.log(element +"," +index);
+// }); → prints a,1 b,2 c,3
+// _.forEach({x: 1, y: 2, z:3}, function(value, key, collection) {
+//  console.log(value +"," +key);
+// }); → prints x,1 y,2 z,3
+_.forEach = function(collection, callback) {
 
 }
 
-_.forEachRight = function(colllection, iteratee) {
+// Iterates over elements of collection in reverse invoking callback for each element.
+// Callback(element/value, index/key, collection)
+// _.forEach(['a','b','c'], function(element, index, collection) {
+//  console.log(element +"," +index);
+// }); → prints c,3 b,2 a,1
+// _.forEach({x: 1, y: 2, z:3}, function(value, key, collection) {
+//  console.log(value +"," +key);
+// }); → prints z,3 y,2 x,1
+_.forEachRight = function(colllection, callback) {
 
 }
 
+// Creates an array of values by running each element in collection through callback
+// Should we explain that map returns?
+// Callback (element/value, index/key, collection)
+// _.map([1,2,3], function(element, index, collection) {
+//  return element * 3;
+// }); -> [3,6,9]
+// _.map({a: 4, b: 5, c: 6}, function(element, index, collection) {
+//  return element * 3;
+// }); -> [12,15,18]
+_.map = function(collection, callback) {
+
+}
+
+// Gets the index at which the first occurrence of value is found in array
+// Returns -1 if element is not in array
+// _.indexOf([11,22,33], 11); → 1
+// _.indexOf([11,22,33], 5); → -1
 _.indexOf = function(array, value) {
 
 }
 
-_.filter = function(collection, predicate) {
+// Iterates over elements of collection, returning an array of all elements callback returns truthy for.
+// _.filter([1,2,3,4], function(element, index, collection) {
+//  return element % 2 === 0;
+// }); → [2,4]
+// _.filter({a: 1, b: 2,c: 3,d: 4}, function(element, index, collection) {
+//  return element % 2 !== 0;
+// }); → [1,3]
+_.filter = function(collection, callback) {
 
 }
 
-_.reject = function(collection, predicate) {
+// Removes all elements from array that callback returns truthy for and returns an array of the removed elements.
+// _.remove([1,2,3,4], function(element, index, collection) {
+//  return element % 2 === 0;
+// }); → [1,3]
+// _.remove({a:1, b:2, c:3, d:4}, function(element, index, collection) {
+//  return element % 2 !== 0;
+// }); → [2,4]
+// Challenge: use _.filter
+_.reject = function(collection, callback) {
 
 }
 
+// Creates a duplicate-value-free version of an array
+// _.uniq([1,2,1]); → [1,2]
 _.uniq = function(array) {
 
 }
 
-_.map = function(collection, iteratee) {
-
-}
-
+// Gets the value of key from all elements in collection.
+// _.pluck([{user: 'Bob', age: 20},{user: 'Sam', age: 25}], 'user'); → ['Bob','Sam']
 _.pluck = function(collection, key) {
 
 }
 
-_.reduce = function() {
+// Reduces collection to a value which is the accumulated result of running each element in collection through iteratee, where each successive invocation is supplied the return value of the previous. If accumulator is not provided the first element of collection is used as the initial value.
+// _.reduce([1,2], function(sum,n) {
+//  return sum + n;
+// }); → 3
+// _.reduce({a:1, b:2}, function(result, n, key) {
+//  result[key] = n*3;
+//  return result;
+// }, {}); → {a:3, b:6}
+_.reduce = function(collection, callback, start) {
 
 }
 
-_.reduceRight = function() {
+// This method is like _.reduce except that it iterates over elements of collection from right to left.
+// _.reduceRight([1,2], function(sum,n) {
+//  return difference - n;
+// }); → 1
+_.reduceRight = function(collection, callback, start) {
 
 }
 
-_.contains = function() {
+// Flattens a nested array.
+// _.flatten([1, [2, 3, [4]]]); → [1, 2, 3, [4]]
+_.flatten = function(array) {
 
 }
 
-_.every = function() {
+// Recursively flattens a nested array.
+// _.flattenDeep([1, [2, 3, [4]]]); → [1, 2, 3, 4]
+_.flattenDeep = function(array) {
 
 }
 
-_.some = function() {
+// Creates a slice of array with n elements dropped from the beginning. n defaults to 1
+// _.drop([1, 2, 3]); → [2, 3]
+// _.drop([1, 2, 3], 2); → [3]
+// _.drop([1, 2, 3], 5); → []
+// _.drop([1, 2, 3], 0); → [1, 2, 3]
+_.drop = function(array, n) {
 
 }
 
-_.flatten = function() {
+// Creates a slice of array with n elements dropped from the end.
+// _.dropRight([1, 2, 3]); → [1, 2]
+// _.dropRight([1, 2, 3], 2); → [1]
+// _.dropRight([1, 2, 3], 5); → []
+// _.dropRight([1, 2, 3], 0); → [1, 2, 3]
+_.dropRight = function(array, n) {
 
 }
 
-_.drop = function() {
+//Creates a slice of array with n elements taken from the beginning.
+// _.take([1, 2, 3]); → [1]
+// _.take([1, 2, 3], 2); → [1, 2]
+// _.take([1, 2, 3], 5); → [1, 2, 3]
+// _.take([1, 2, 3], 0); → []
+_.take = function(array, n) {
 
 }
 
-_.dropRight = function() {
-
-}
-
-_.take = function() {
-
-}
-
-_.takeRight = function() {
-
-}
-
-_.includes = function() {
+// Checks if value is in collection.
+// _.includes([1, 2, 3], 1); → true
+// _.includes([1, 2, 3], 5); → false
+// _.includes({ 'user': 'fred', 'age': 40 }, 'fred'); → true
+// _.includes('pebbles', 'eb'); → true
+_.includes = function(array, target) {
 
 }
 
 // Object
 
+// Assigns own enumerable properties of source object(s) to the destination object. Subsequent sources overwrite property assignments of previous sources.
+// _.extend({ 'user': 'barney' }, { 'age': 40 }, { 'user': 'fred' }); → { 'user': 'fred', 'age': 40 }
 _.extend = function() {
 
 }
 
+// Creates a clone of value.
+// var users = [{ 'user': 'barney' },{ 'user': 'fred' }];
+// var shallowClone = _.clone(users);
+// shallowClone[0].user === users[0].user → true
+// shallowClone === users → true
 _.clone = function() {
 
 }
 
+// Creates a deep clone of value.
+// var users = [{ 'user': 'barney' },{ 'user': 'fred' }];
+// var deepClone = _.cloneDeep(users)
+// deepClone[0].user === users[0].user → false
+// deepClone === users → true
 _.cloneDeep = function() {
 
 }
 
-
-
 // Function
 
+// Invokes the method named by methodName on each element in collection, returning an array of the results of each invoked method.
 _.invoke = function() {
 
 }
 
-_.once = function() {
+// Creates a function that is restricted to invoking func once. Repeat calls to the function return the value of the first call.
+//
+_.once = function(func) {
 
 }
 
-_.memoize = function() {
+// Creates a function that memoizes the result of func.
+_.memoize = function(func) {
 
 }
 
-_.delay = function() {
+// Invokes func after wait milliseconds. Any additional arguments are provided to func when it is invoked.
+_.delay = function(func, wait) {
 
 }
 
+// Creates a function that only invokes func at most once per every wait milliseconds.
+//
 _.throttle = function() {
 
 }
