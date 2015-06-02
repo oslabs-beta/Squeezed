27d736f94cf7afc6512288cbe7021b47d9bf095d
Alex Zai <azai91@gmail.com>
2015-06-01 19:14:53 -0700

    (feat) edit extends test

--- test/index.js
+++ test/index.js
@@ -343,27 +343,28 @@ describe('extend', function() {
   it('should extend an object with the attributes of another', function() {
     var to = {};
     var from = {a:'b'};
-    var extended = extend(to, from);
-    expect(extended.a).to.equal('b');
+    extend(to, from);
+    expect(to.a).to.equal('b');
   });
 
   it('should override properties found on the destination', function() {
     var to = {a:'x'};
     var from = {a:'b'};
-    var extended = extend(to, from);
-    expect(extended.a).to.equal('b');
+    extend(to, from)
+    expect(to.a).to.equal('b');
   });
 
   it('should not override properties not found in the source', function() {
     var to = {x:'x'};
     var from = {a:'b'};
-    var extended = extend(to, from);
-    expect(extended.x).to.equal('x');
+    extend(to, from);
+    expect(to.x).to.equal('x');
   });
 
   it('should extend from multiple source objects', function() {
-    var extended = extend({x:1}, {a:2}, {b:3});
-    expect(extended).to.eql({x:1, a:2, b:3});
+    var first = {x:1};
+    extend(first, {a:2}, {b:3});
+    expect(first).to.eql({x:1, a:2, b:3});
   });
 
   it('in the case of a conflict, it should use the last property\'s values when extending from multiple source objects', function() {
