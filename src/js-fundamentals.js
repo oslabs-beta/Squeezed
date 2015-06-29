e2be5862156e53fd764236aa51a345ae1193987c
Alex Zai <azai91@gmail.com>
2015-06-29 12:36:28 -0700

    (fix) update test

--- src/js-fundamentals.js
+++ src/js-fundamentals.js
@@ -405,3 +405,18 @@ function after(count, func) {
 function before(count, func) {
 
 }
+
+/**
+ * returns a javascript object from a JSON formatted string
+ * Example json to javascript object:
+ *   "5"          -> 5
+ *   "'hello'"   -> 'hello'
+ *   "[]"         -> []
+ *   "{}"         -> {}
+ *   "["hello"]"  -> ['hello']
+ *
+ *  For more examples, use the JSON.stringify method in the console
+ */
+function JSONParser(string) {
+
+}
--- test/index.js
+++ test/index.js
@@ -30,7 +30,7 @@ describe('isNegative', function() {
 });
 
 describe('isNegativeOrOdd', function() {
-  it('should return true if number is negative', function() {
+  it('should return true if number is odd', function() {
     expect(isNegativeOrOdd(1)).to.eql(true);
     expect(isNegativeOrOdd(3)).to.eql(true);
     expect(isNegativeOrOdd(5)).to.eql(true);
@@ -838,4 +838,51 @@ describe('before', function() {
     expect(beforeIncr()).to.eql(1);
     expect(beforeIncr()).to.eql(1);
   });
+});
+
+describe('JSONParser', function() {
+  it('should work on numbers', function() {
+    expect(JSONParser(JSON.stringify(1))).to.eql(1);
+  });
+
+  it('should work on strings', function() {
+    expect(JSONParser(JSON.stringify('test'))).to.eql('test');
+  });
+
+  it('should work on booleans', function() {
+    expect(JSONParser(JSON.stringify(true))).to.eql(true);
+  });
+
+  it('should work on arrays', function() {
+    expect(JSONParser(JSON.stringify([]))).to.eql([]);
+    expect(JSONParser(JSON.stringify(['a']))).to.eql(['a']);
+    expect(JSONParser(JSON.stringify([1]))).to.eql([1]);
+    expect(JSONParser(JSON.stringify([true]))).to.eql([true]);
+    expect(JSONParser(JSON.stringify([true,1]))).to.eql([true,1]);
+    expect(JSONParser(JSON.stringify([true,1,'test']))).to.eql([true,1,'test']);
+  });
+
+  it('should work on objects', function() {
+    expect(JSONParser(JSON.stringify({}))).to.eql({});
+    expect(JSONParser(JSON.stringify({a:true}))).to.eql({a:true});
+    expect(JSONParser(JSON.stringify({b:1}))).to.eql({b:1});
+    expect(JSONParser(JSON.stringify({c:'test'}))).to.eql({c:'test'})
+    expect(JSONParser(JSON.stringify({a:true,b:1}))).to.eql({a:true,b:1});
+    expect(JSONParser(JSON.stringify({a:true,b:1,c:'test'}))).to.eql({a:true,b:1,c:'test'});
+  });
+
+  it('should working with nesting in objects', function() {
+    expect(JSONParser(JSON.stringify({a:{}}))).to.eql({a:{}});
+    expect(JSONParser(JSON.stringify({a:{b:1}}))).to.eql({a:{b:1}});
+    expect(JSONParser(JSON.stringify({a:{b:1,c:2}}))).to.eql({a:{b:1,c:2}});
+    expect(JSONParser(JSON.stringify({a:{b:1},c:2}))).to.eql({a:{b:1},c:2});
+    expect(JSONParser(JSON.stringify({a:{b:{c:2}}}))).to.eql({a:{b:{c:2}}});
+  });
+
+  it('should working with nesting in arrays', function() {
+    expect(JSONParser(JSON.stringify([{a:1}]))).to.eql([{a:1}]);
+    expect(JSONParser(JSON.stringify([{a:1},{b:2}]))).to.eql([{a:1},{b:2}]);
+    expect(JSONParser(JSON.stringify([{a:{c:2}},{b:2}]))).to.eql([{a:{c:2}},{b:2}]);
+  });
+
 });
