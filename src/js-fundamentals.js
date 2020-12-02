ba123471cc56cde11761eb0893bd490741be9c64
Shane Taylor <shaneallantaylor@gmail.com>
2020-12-01 19:12:56 -0800

    Update js-fundamentals.js

--- src/js-fundamentals.js
+++ src/js-fundamentals.js
@@ -190,7 +190,7 @@ function map(array, callback) {
  *  return element % 2 === 0;
  * }); → [2,4]
  * filter({a: 1, b: 2,c: 3,d: 4}, (value, key, collection) => {
- *  return element % 2 !== 0;
+ *  return value % 2 !== 0;
  * }); → {a: 1, c: 3}
  */
 function filter(collection, callback) {
@@ -203,7 +203,7 @@ function filter(collection, callback) {
  *  return element % 2 === 0;
  * }); → [1,3]
  * reject({a:1, b:2, c:3, d:4}, (value, key, collection) => {
- *  return element % 2 !== 0;
+ *  return value % 2 !== 0;
  * }); → {b:2, d:4}
  * Challenge: use filter
  */
