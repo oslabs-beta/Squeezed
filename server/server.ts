516b261a1c1e021401e2687c2c216b339e2e036a
alexarobertss <alexarobertss@protonmail.com>
2022-07-12 15:23:17 -0400

    setup mod ts, frontend layout started

--- server/server.ts
+++ server/server.ts
@@ -1,10 +1,17 @@
 import { Application } from "https://deno.land/x/oak/mod.ts";
-const port = 8000;
+const port = 8080;
 const app = new Application();
+// import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
+import { handleRequest } from "../mod.ts"
+
 
 app.use((ctx) => {
-  ctx.response.body = "Hello World!";
+  ctx.response.body = handleRequest;
 });
+
+// app.use('/', (req, res, next) => {
+//   res.type("text/html").send(html);
+// });
 
 app.addEventListener('listen', () => {
     console.log(`Listening on localhost:${port}`);
