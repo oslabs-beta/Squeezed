3af01a853128b5169e704b48dfc7478ffb61fbc1
Carol Xia <c.xia.98@gmail.com>
2022-07-20 20:52:57 -0700

    updated create table queries and saved non-existing project to db in projectController

--- server/controllers/projectController.ts
+++ server/controllers/projectController.ts
@@ -1,30 +1,102 @@
+// import db from '../db.ts'
+// import Dex from "https://deno.land/x/dex/mod.ts";
+// const dex = Dex({client: 'postgres'}); 
+
+// const projectController: any = {};
+
+// projectController.getproject = async(ctx: any) => {
+//     try {
+//         const id = await ctx.params.id;
+//         const data = await db.queryObject({
+//           text: `SELECT * FROM projects WHERE account_id = ${id}`
+//         });
+//         ctx.response.body = data.rows[0]
+//         ctx.response.status = 200;
+//         return;
+//     } catch (err) {
+//         ctx.response.body = { status: false, data: null};
+//         ctx.response.status = 500;
+//         console.log(err);         
+//     }
+// };
+
+// projectController.saveProject = async(ctx: any) => {
+//     try {
+//         const { value } = await ctx.request.body({type: 'json'});
+//         // console.log(ctx.request.body, value);
+//         let selectQuery = dex("projects").insert(await value).toString();
+//         const data = await db.queryObject(selectQuery);
+//         ctx.response.status = 200; 
+//         return; 
+//     } catch (err) {
+//         ctx.response.body = { status: false, data: null};
+//         ctx.response.status = 500;
+//         console.log(err);
+//     }
+// };
+
+// export default projectController;
+
+
 import db from '../db.ts'
 import Dex from "https://deno.land/x/dex/mod.ts";
 const dex = Dex({client: 'postgres'}); 
 
 const projectController: any = {};
 
-projectController.getproject = async(ctx: any) => {
-    try {
-        const id = await ctx.params.id;
-        const data = await db.queryObject({
-          text: `SELECT * FROM projects WHERE account_id = ${id}`
-        });
-        ctx.response.body = data.rows[0]
-        ctx.response.status = 200;
-        return;
-    } catch (err) {
-        ctx.response.body = { status: false, data: null};
-        ctx.response.status = 500;
-        console.log(err);         
-    }
-};
+// projectController.getproject = async(ctx: any) => {
+//     try {
+//         const id = await ctx.params.id;
+//         const data = await db.queryObject({
+//           text: `SELECT * FROM projects WHERE account_id = ${id}`
+//         });
+//         ctx.response.body = data.rows[0]
+//         ctx.response.status = 200;
+//         return;
+//     } catch (err) {
+//         ctx.response.body = { status: false, data: null};
+//         ctx.response.status = 500;
+//         console.log(err);         
+//     }
+// };
 
 projectController.saveProject = async(ctx: any) => {
     try {
+        //get request body and store in constants
         const { value } = await ctx.request.body({type: 'json'});
-        let selectQuery = dex("projects").insert(await value).toString();
-        const data = await db.queryObject(selectQuery);
+        const obj = await value;
+        let { project_id, elementsArr } = obj;
+
+        //if project doesn't exist in db, create a new row in projects table
+        if(!project_id){
+            let insertQuery = dex("projects").insert({}).returning('id').toString(); 
+            const newData = await db.queryObject(insertQuery);
+            project_id = newData.rows[0].id;
+        }
+        console.log(project_id);
+
+        //project doesn't exist, add row to projects table and add its elements to table
+        // if(!data.rows.length){
+        //         let insertQuery = dex("projects").insert(await value).toString(); 
+        //         const newData = await db.queryObject(insertQuery);
+        //         // console.log(data);
+        //     }
+
+        //project does exist
+        //loop through elementsArr, if element already exists, delete from table and create again. if doesn't, create new row
+        elementsArr.forEach(async element => {
+
+            const text = element.text;
+            const textAlign = element.textAlign;
+            const textDecoration = element.textDecoration;
+            const backgroundColor = element.backgroundColor;
+            const color = element.color;
+            const margin = element.margin;
+            const height = element.height;
+            const padding = element.padding;
+            let selectQuery = dex("elements").insert(await text, textAlign, textDecoration, backgroundColor, color, margin, height, padding, project_id).toString();
+            await db.queryObject(selectQuery);
+        })
         ctx.response.status = 200; 
         return; 
     } catch (err) {
@@ -35,3 +107,9 @@ projectController.saveProject = async(ctx: any) => {
 };
 
 export default projectController;
+
+
+// //create project in db
+// const projectQuery = dex.select().from("projects").where({id: project_id}).toString();
+// const data = await db.queryObject(projectQuery);
+// console.log(data.rows);
