dd3e3c0aa4afcd582e227affe977433687f39b53
Carol Xia <c.xia.98@gmail.com>
2022-08-01 08:22:05 -0700

    moved elementsList from sidebar to utils folder

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,5 +1,6 @@
 import { React } from '../deps.tsx';
 import { IHtmlElement, IProps, ISideBarProps } from './../utils/types.ts';
+import elementsList from './../utils/elementsList.js';
 
 const SideBar: React.FC<ISideBarProps> = (props: ISideBarProps) => {
   const { elementsArr, setElementsArr, currentElement, setCurrentElement } = props;
@@ -38,12 +39,9 @@ const SideBar: React.FC<ISideBarProps> = (props: ISideBarProps) => {
   const handleDrop = (event: React.DragEvent<HTMLDivElement>) => {
     const area = event.dataTransfer.getData("area");
     const newElementsArr = [...elementsArr];
-    // console.log("before if statement", newElementsArr);
  
     if(area === "dragArea"){
       const id = event.dataTransfer.getData("id");
-      // console.log('new el', id)
-      // console.log('new el elArr length', elementsArr.length);
       setContent(id);
       const newElement = {
         id: elementsArr.length,      
@@ -71,7 +69,7 @@ const SideBar: React.FC<ISideBarProps> = (props: ISideBarProps) => {
       newElementsArr.splice(dragOverItem.current, 0, dragItemContent);
       dragItem.current = null;
       dragOverItem.current = null;
-      console.log("new arr after drop:", newElementsArr);
+      // console.log("new arr after drop:", newElementsArr);
       reorderElArr(newElementsArr);
       setElementsArr(newElementsArr);
     }
@@ -132,108 +130,8 @@ const SideBar: React.FC<ISideBarProps> = (props: ISideBarProps) => {
       .style
       .backgroundColor = 'yellow';
   }
-
-  const elementsList = 
-    [
-      {
-        id: 'div',
-        element: 'DIV',
-        backgroundColor: 'rgb(142,233,172)'
-      },
-      {
-        id: 'paragraph',
-        element: 'PARAGRAPH',
-        backgroundColor: 'rgb(148,233,168)'
-      },
-      {
-        id: 'button',
-        element: 'BUTTON',
-        backgroundColor: 'rgb(152,233,166)'
-      },
-      {
-        id: 'img',
-        element: 'IMAGE',
-        backgroundColor: 'rgb(158,233,163)'
-      },
-      {
-        id: 'h1',
-        element: 'HEADER 1',
-        backgroundColor: 'rgb(163,233,160)'
-      },
-      {
-        id: 'h2',
-        element: 'HEADER 2',
-        backgroundColor: 'rgb(168,233,158)'
-      },
-      {
-        id: 'h3',
-        element: 'HEADER 3',
-        backgroundColor: 'rgb(173,233,155)'
-      },
-      {
-        id: 'footer',
-        element: 'FOOTER',
-        backgroundColor: 'rgb(178,233,152)'
-      },
-      {
-        id: 'ol',
-        element: 'LIST (OL)',
-        backgroundColor: 'rgb(187,233,147)'
-      },
-      {
-        id: 'ul',
-        element: 'LIST (UL)',
-        backgroundColor: 'rgb(196,233,143)'
-      },
-      {
-        id: 'input',
-        element: 'INPUT',
-        backgroundColor: 'rgb(202,233,139)'
-      },
-      {
-        id: 'link',
-        element: 'LINK',
-        backgroundColor: 'rgb(207,233,137)'
-      },
-      {
-        id: 'label',
-        element: 'LABEL',
-        backgroundColor: 'rgb(212,233,134)'
-      },
-      {
-        id: 'span',
-        element: 'SPAN',
-        backgroundColor: 'rgb(218,233,131)'
-      },
-      {
-        id: 'button',
-        element: 'LIST (UL)',
-        backgroundColor: 'rgb(220,233,129)'
-      },
-      {
-        id: 'form',
-        element: 'FORM',
-        backgroundColor: 'rgb(222,233,128)'
-      },
-      {
-        id: 'menu',
-        element: 'MENU',
-        backgroundColor: 'rgb(227,233,126)'
-      },
-      {
-        id: 'title',
-        element: 'TITLE',
-        backgroundColor: 'rgb(232,233,123)'
-      },
-      {
-        id: 'area',
-        element: 'AREA',
-        backgroundColor: 'rgb(238,233,120)'
-      }
-    ];
     
     const renderElementsList = elementsList.map((el: { id: string, element: string, backgroundColor: string}) => {
-      // console.log(el, ind);
       return (
         <div id={el.id} onDragStart={(e) => handleDragStart(e, 'dragArea')}>
           <button style={{ backgroundColor: el.backgroundColor, color: "#2d3033", width: "100%", fontSize: '20px',fontWeight: 'bolder'}} draggable="true">
@@ -245,13 +143,10 @@ const SideBar: React.FC<ISideBarProps> = (props: ISideBarProps) => {
     });
   
     const createdElements = elementsArr.map((el: IHtmlElement, index: number) => {
-      // console.log("html tags: ", elementsArr[index], index);
       return (
         <div 
           draggable='true'
           onDrop={handleDrop}
-          // onDragEnter={handleDragOverStart}
-          // onDragLeave={handleDragOverEnd}
           onDragStart={(e) => handleDragStart(e, 'dropArea')}
           onDragEnter={(e) => {dragEnter(e, index)}}
           className="draggedTags"
--- dist/run/static/js/main.js
+++ dist/run/static/js/main.js
@@ -7109,6 +7109,103 @@ const mod1 = {
     unstable_renderSubtreeIntoContainer: Hf,
     version: Wf
 };
+const elementsList = [
+    {
+        id: 'div',
+        element: 'DIV',
+        backgroundColor: 'rgb(142,233,172)'
+    },
+    {
+        id: 'paragraph',
+        element: 'PARAGRAPH',
+        backgroundColor: 'rgb(148,233,168)'
+    },
+    {
+        id: 'button',
+        element: 'BUTTON',
+        backgroundColor: 'rgb(152,233,166)'
+    },
+    {
+        id: 'img',
+        element: 'IMAGE',
+        backgroundColor: 'rgb(158,233,163)'
+    },
+    {
+        id: 'h1',
+        element: 'HEADER 1',
+        backgroundColor: 'rgb(163,233,160)'
+    },
+    {
+        id: 'h2',
+        element: 'HEADER 2',
+        backgroundColor: 'rgb(168,233,158)'
+    },
+    {
+        id: 'h3',
+        element: 'HEADER 3',
+        backgroundColor: 'rgb(173,233,155)'
+    },
+    {
+        id: 'footer',
+        element: 'FOOTER',
+        backgroundColor: 'rgb(178,233,152)'
+    },
+    {
+        id: 'ol',
+        element: 'LIST (OL)',
+        backgroundColor: 'rgb(187,233,147)'
+    },
+    {
+        id: 'ul',
+        element: 'LIST (UL)',
+        backgroundColor: 'rgb(196,233,143)'
+    },
+    {
+        id: 'input',
+        element: 'INPUT',
+        backgroundColor: 'rgb(202,233,139)'
+    },
+    {
+        id: 'link',
+        element: 'LINK',
+        backgroundColor: 'rgb(207,233,137)'
+    },
+    {
+        id: 'label',
+        element: 'LABEL',
+        backgroundColor: 'rgb(212,233,134)'
+    },
+    {
+        id: 'span',
+        element: 'SPAN',
+        backgroundColor: 'rgb(218,233,131)'
+    },
+    {
+        id: 'button',
+        element: 'LIST (UL)',
+        backgroundColor: 'rgb(220,233,129)'
+    },
+    {
+        id: 'form',
+        element: 'FORM',
+        backgroundColor: 'rgb(222,233,128)'
+    },
+    {
+        id: 'menu',
+        element: 'MENU',
+        backgroundColor: 'rgb(227,233,126)'
+    },
+    {
+        id: 'title',
+        element: 'TITLE',
+        backgroundColor: 'rgb(232,233,123)'
+    },
+    {
+        id: 'area',
+        element: 'AREA',
+        backgroundColor: 'rgb(238,233,120)'
+    }
+];
 const SideBar = (props)=>{
     const { elementsArr , setElementsArr , currentElement , setCurrentElement  } = props;
     const { setInputText , setTextAlign , setTextDecoration , setBackgroundColor , setColor , setMargin , setWidth , setHeight , setPadding , setFontSize , setClassName  } = props;
@@ -7167,7 +7264,6 @@ const SideBar = (props)=>{
             newElementsArr.splice(dragOverItem.current, 0, dragItemContent);
             dragItem.current = null;
             dragOverItem.current = null;
-            console.log("new arr after drop:", newElementsArr);
             reorderElArr(newElementsArr);
             setElementsArr(newElementsArr);
         }
@@ -7212,103 +7308,6 @@ const SideBar = (props)=>{
             el.id = ind;
         });
     };
-    const elementsList = [
-        {
-            id: 'div',
-            element: 'DIV',
-            backgroundColor: 'rgb(142,233,172)'
-        },
-        {
-            id: 'paragraph',
-            element: 'PARAGRAPH',
-            backgroundColor: 'rgb(148,233,168)'
-        },
-        {
-            id: 'button',
-            element: 'BUTTON',
-            backgroundColor: 'rgb(152,233,166)'
-        },
-        {
-            id: 'img',
-            element: 'IMAGE',
-            backgroundColor: 'rgb(158,233,163)'
-        },
-        {
-            id: 'h1',
-            element: 'HEADER 1',
-            backgroundColor: 'rgb(163,233,160)'
-        },
-        {
-            id: 'h2',
-            element: 'HEADER 2',
-            backgroundColor: 'rgb(168,233,158)'
-        },
-        {
-            id: 'h3',
-            element: 'HEADER 3',
-            backgroundColor: 'rgb(173,233,155)'
-        },
-        {
-            id: 'footer',
-            element: 'FOOTER',
-            backgroundColor: 'rgb(178,233,152)'
-        },
-        {
-            id: 'ol',
-            element: 'LIST (OL)',
-            backgroundColor: 'rgb(187,233,147)'
-        },
-        {
-            id: 'ul',
-            element: 'LIST (UL)',
-            backgroundColor: 'rgb(196,233,143)'
-        },
-        {
-            id: 'input',
-            element: 'INPUT',
-            backgroundColor: 'rgb(202,233,139)'
-        },
-        {
-            id: 'link',
-            element: 'LINK',
-            backgroundColor: 'rgb(207,233,137)'
-        },
-        {
-            id: 'label',
-            element: 'LABEL',
-            backgroundColor: 'rgb(212,233,134)'
-        },
-        {
-            id: 'span',
-            element: 'SPAN',
-            backgroundColor: 'rgb(218,233,131)'
-        },
-        {
-            id: 'button',
-            element: 'LIST (UL)',
-            backgroundColor: 'rgb(220,233,129)'
-        },
-        {
-            id: 'form',
-            element: 'FORM',
-            backgroundColor: 'rgb(222,233,128)'
-        },
-        {
-            id: 'menu',
-            element: 'MENU',
-            backgroundColor: 'rgb(227,233,126)'
-        },
-        {
-            id: 'title',
-            element: 'TITLE',
-            backgroundColor: 'rgb(232,233,123)'
-        },
-        {
-            id: 'area',
-            element: 'AREA',
-            backgroundColor: 'rgb(238,233,120)'
-        }
-    ];
     const renderElementsList = elementsList.map((el)=>{
         return mod.createElement("div", {
             id: el.id,
